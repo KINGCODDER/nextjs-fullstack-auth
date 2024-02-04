87b753c3934886671dbbaf8d6fa1eeae8056e0cd
UmangSachdeva <umang965454@gmail.com>
2024-02-04 22:45:24 +0530

    reinstalled packages

--- src/app/test/Stars.jsx
+++ src/app/test/Stars.jsx
@@ -1,12 +1,12 @@
 import { useRef, Suspense, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-// import * as random from "maath/random/dist/maath-random.esm";
+import * as random from "maath/random/dist/maath-random.esm";
 import { useControls } from "leva";
 
 export default function Stars(props) {
-  // const ref = useRef();
-  // const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
   // useFrame((state, delta) => {
   //   ref.current.rotation.x -= delta / 20;
